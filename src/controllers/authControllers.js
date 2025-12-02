406625d6247629fa08047cce81bb3254b7a6d280
Zibnu <crimhomid@gmail.com>
2025-12-02 20:37:02 +0700

    add role for create account admin

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -9,7 +9,7 @@
 // Register
   exports.register = async(req, res) => {
     try {
-      const { username, email, password} = req.body;
+      const { username, email, password, role} = req.body;
       // validasi input
       if(!username || !email || !password) {
         return res.status(400).json({
@@ -35,7 +35,7 @@
         username,
         email,
         password : hashPassword,
-        role : "user",
+        role : role,
         isActive : true,
       });
 
